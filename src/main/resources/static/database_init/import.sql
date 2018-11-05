fb260344da5d7f517bf5324093772b97f7d0ec40
XP256 <zhuxinpeng1995@gmail.com>
2018-11-05 13:47:44 -0500

    modified database initialize sql file

--- src/main/resources/static/database_init/import.sql
+++ src/main/resources/static/database_init/import.sql
@@ -128,10 +128,10 @@ create table user_info(
     primary key(id)
 )comment 'User';
 
-INSERT INTO user_info(id,active,address,email,name,password,phone,role) VALUES (2147483641, true, '3200 West Road', 'customer1@email.com', 'customer1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', '123456789', 'ROLE_CUSTOMER');
-INSERT INTO user_info(id,active,address,email,name,password,phone,role) VALUES (2147483642, true, '2000 John Road', 'manager1@email.com', 'manager1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', '987654321', 'ROLE_MANAGER');
-INSERT INTO user_info(id,active,address,email,name,password,phone,role) VALUES (2147483643, true, '222 East Drive ', 'employee1@email.com', 'employee1', '$2a$10$PrI5Gk9L.tSZiW9FXhTS8O8Mz9E97k2FZbFvGFFaSsiTUIl.TCrFu', '123123122', 'ROLE_EMPLOYEE');
-INSERT INTO user_info(id,active,address,email,name,password,phone,role) VALUES (2147483645, true, '3100 Western Road A', 'customer2@email.com', 'customer2', '$2a$10$0oho5eUbDqKrLH026A2YXuCGnpq07xJpuG/Qu.PYb1VCvi2VMXWNi', '2343456', 'ROLE_CUSTOMER');
+INSERT INTO user_info(id,active,address,email,name,password,phone,role) VALUES (2147483641, true, '3200 West Road', 'customer1@email.com', 'customer1', '123', '123456789', 'ROLE_CUSTOMER');
+INSERT INTO user_info(id,active,address,email,name,password,phone,role) VALUES (2147483642, true, '2000 John Road', 'manager1@email.com', 'manager1', '123', '987654321', 'ROLE_MANAGER');
+INSERT INTO user_info(id,active,address,email,name,password,phone,role) VALUES (2147483643, true, '222 East Drive ', 'employee1@email.com', 'employee1', '123', '123123122', 'ROLE_EMPLOYEE');
+INSERT INTO user_info(id,active,address,email,name,password,phone,role) VALUES (2147483645, true, '3100 Western Road A', 'customer2@email.com', 'customer2', '123', '2343456', 'ROLE_CUSTOMER');
 
 
 
